b79ba1b2471974b7a9163f8d0b4a9a0fddac0c45
parkjoungwan <whddhs6645@gmail.com>
2022-03-01 11:19:41 +0900

    cpp0_ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+char	ft_upper(char c)
+{
+	if (c >= 97 && c <= 122)
+		c = c - 32;
+	return (c);
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+		for (int i = 1; i < argc; i++)
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << ft_upper(argv[i][j]);
+	std::cout << "\n";
+	return (0);
+}
